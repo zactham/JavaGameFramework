d18f03b05bc5a43073167e5ee447602d30e5bf14
zactham <zactham@gmail.com>
2017-06-29 12:25:21 -0400

    Add files via upload

--- TitleScreen.java
+++ TitleScreen.java
@@ -1,7 +1,8 @@
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.ImageIcon;
@@ -9,17 +10,18 @@ import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import java.awt.event.KeyListener;
 
 
-public class TitleScreen extends JApplet //implements MouseListener
+public class TitleScreen extends JApplet implements KeyListener
 {
 
 	public static boolean easy = false;
 	public static boolean med = false;
 	public static boolean hard = false;
 
-	private WMBoard board;
-	
+	private SnakeGame board;
+
 	private ImageIcon titleScreenImage;//image
 
 
@@ -43,27 +45,27 @@ public class TitleScreen extends JApplet //implements MouseListener
 		}
 		catch(Exception err)
 		{
-			
+
 		}
 
 
 		//Adds the image and creates a button out of it
-		titleScreenImage = new ImageIcon(this.getClass().getResource("Pictures/EasyMediumHard.jpg"));//image	
+		titleScreenImage = new ImageIcon(this.getClass().getResource("TSImage.jpg"));//image	
 		titleScButton = new JButton (titleScreenImage);//image button
 		getContentPane().add(titleScButton);
-		setSize(833,777);
+		setSize(320,240);
 		centerWindow();//centers the window
 
-		
-		//Based on where they click in easy medium or hard something happens
-		titleScButton.addMouseListener(new MouseAdapter()
-		{
-			public void mouseClicked(MouseEvent e)
-			{
-				//System.out.println(e);
-				//EASY
 
-				if (e.getX() > 41 && e.getX() < 266 && e.getY() > 253 && e.getY() < 353)
+		titleScButton.addKeyListener(new KeyAdapter()
+		{
+			public void keyPressed(KeyEvent arg0) 
+			{
+				// TODO Auto-generated method stub
+				int c = arg0.getKeyCode();
+
+				//Pressing the keys 1 2 3 on the num pad on the right side of the keyboard
+				if (c == KeyEvent.VK_E)
 				{
 					//System.out.println("Easy");
 
@@ -72,7 +74,7 @@ public class TitleScreen extends JApplet //implements MouseListener
 					soundPlaying = false;
 
 
-					board = new WMBoard();
+					board = new SnakeGame();
 					try
 					{
 						board.init();
@@ -82,10 +84,7 @@ public class TitleScreen extends JApplet //implements MouseListener
 					}
 
 				}
-
-				//MEDIUM
-				//page.fillOval(112, 15, 75, 75);
-				if (e.getX() > 312 && e.getX() < 536 && e.getY() > 253 && e.getY() < 353)
+				if (c == KeyEvent.VK_M)
 				{
 					//System.out.println("Medium");
 
@@ -94,8 +93,8 @@ public class TitleScreen extends JApplet //implements MouseListener
 					Sound.audioClip.stop();
 					soundPlaying = false;
 
-					
-					board = new WMBoard();
+
+					board = new SnakeGame();
 					try
 					{
 						board.init();
@@ -104,10 +103,7 @@ public class TitleScreen extends JApplet //implements MouseListener
 						System.out.println(e1);
 					}
 				}
-
-				//HARD
-				//page.fillOval(212, 15, 75, 75);
-				if (e.getX() > 579 && e.getX() < 803 && e.getY() > 253 && e.getY() < 353)
+				if (c == KeyEvent.VK_H)
 				{
 					//System.out.println("Hard");
 
@@ -116,8 +112,8 @@ public class TitleScreen extends JApplet //implements MouseListener
 					Sound.audioClip.stop();
 					soundPlaying = false;
 
-					
-					board = new WMBoard();
+
+					board = new SnakeGame();
 					try
 					{
 						board.init();
@@ -125,9 +121,16 @@ public class TitleScreen extends JApplet //implements MouseListener
 					{
 						//System.out.println(e1);
 					}
-
 				}
+			}
+		});
 
+
+		//Based on where they click in easy medium or hard something happens
+		titleScButton.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e)
+			{
 				//Music Toggle
 				if (e.getX() > 0 && e.getX() < 27 && e.getY() > 745 && e.getY() < 769)
 				{
@@ -224,6 +227,24 @@ public class TitleScreen extends JApplet //implements MouseListener
 			window.pack();
 			window.setLocationRelativeTo(null);					
 		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
 
